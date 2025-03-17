8b4398ab49881e0331417a003e30f82b7bb3c1d8
yangyile <gogit@github.com/go-xlan/gogit>
2025-03-18 00:58:37 +0700

    简单加点测试代码

--- repo_test.go
+++ repo_test.go
@@ -12,12 +12,10 @@ import (
 func TestRepo_NewGormRepo(t *testing.T) {
 	db := caseDB
 
-	accountRepo := gormrepo.NewRepo(gormclass.Use(&Account{}))
+	repo := gormrepo.NewRepo(gormclass.Use(&Account{}))
 
 	{
-		repo := accountRepo.NewGormRepo(db)
-
-		res, err := repo.FirstX(func(db *gorm.DB, cls *AccountColumns) *gorm.DB {
+		res, err := repo.NewGormRepo(db).FirstX(func(db *gorm.DB, cls *AccountColumns) *gorm.DB {
 			return db.Where(cls.Username.Eq("demo-1-username"))
 		})
 		require.NoError(t, err)
@@ -25,9 +23,53 @@ func TestRepo_NewGormRepo(t *testing.T) {
 	}
 
 	require.NoError(t, db.Transaction(func(db *gorm.DB) error {
-		repo := accountRepo.NewGormRepo(db)
+		res, err := repo.NewGormRepo(db).FirstX(func(db *gorm.DB, cls *AccountColumns) *gorm.DB {
+			return db.Where(cls.Username.Eq("demo-2-username"))
+		})
+		require.NoError(t, err)
+		require.Equal(t, "demo-2-nickname", res.Nickname)
+		return nil
+	}))
+}
 
-		res, err := repo.FirstX(func(db *gorm.DB, cls *AccountColumns) *gorm.DB {
+func TestRepo_GormRepo(t *testing.T) {
+	db := caseDB
+
+	repo := gormrepo.NewRepo(gormclass.Use(&Account{}))
+
+	{
+		res, err := repo.GormRepo(db).FirstX(func(db *gorm.DB, cls *AccountColumns) *gorm.DB {
+			return db.Where(cls.Username.Eq("demo-1-username"))
+		})
+		require.NoError(t, err)
+		require.Equal(t, "demo-1-nickname", res.Nickname)
+	}
+
+	require.NoError(t, db.Transaction(func(db *gorm.DB) error {
+		res, err := repo.GormRepo(db).FirstX(func(db *gorm.DB, cls *AccountColumns) *gorm.DB {
+			return db.Where(cls.Username.Eq("demo-2-username"))
+		})
+		require.NoError(t, err)
+		require.Equal(t, "demo-2-nickname", res.Nickname)
+		return nil
+	}))
+}
+
+func TestRepo_Gorm(t *testing.T) {
+	db := caseDB
+
+	repo := gormrepo.NewRepo(gormclass.Use(&Account{}))
+
+	{
+		res, err := repo.Gorm(db).FirstX(func(db *gorm.DB, cls *AccountColumns) *gorm.DB {
+			return db.Where(cls.Username.Eq("demo-1-username"))
+		})
+		require.NoError(t, err)
+		require.Equal(t, "demo-1-nickname", res.Nickname)
+	}
+
+	require.NoError(t, db.Transaction(func(db *gorm.DB) error {
+		res, err := repo.Gorm(db).FirstX(func(db *gorm.DB, cls *AccountColumns) *gorm.DB {
 			return db.Where(cls.Username.Eq("demo-2-username"))
 		})
 		require.NoError(t, err)
